8bfb4f77bb6587ad9e3474f974c9736ae85951e4
Harry Vicaradge <harryvic_@outlook.com>
2025-10-18 20:54:57 +0100

    added database functionality

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
+
 from flask import Flask, render_template, request
+import sqlite3
+import validators
+from bias_checker import bias_checker
 
 app = Flask(__name__)
 
+def get_sentiment_for_url(url):
+    conn = sqlite3.connect('website_sentiment.db')
+    c = conn.cursor()
+    c.execute("SELECT bias, political_leaning, attitude FROM sentiment_results WHERE url = ?", (url,))
+    result = c.fetchone()
+    conn.close()
+    if result:
+        return result
+    return None
 
-url = 'hello'
-@app.route("/", methods=['GET', 'POST'])
+def add_sentiment_for_url(url, bias, political_leaning, attitude):
+    conn = sqlite3.connect('website_sentiment.db')
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO sentiment_results (url, bias, political_leaning, attitude) VALUES (?, ?, ?, ?)", (url, bias, political_leaning, attitude))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        # URL already exists
+        pass
+    finally:
+        conn.close()
+
+@app.route('/', methods=['POST','GET'])
 def index():
     url = request.form.get('url')
-    return render_template('index.html', url = url)
+    print(validators.url(str(url)))
+    if validators.url(url) != True:
+        return render_template('index.html', url = 'Not a  url')
+    if get_sentiment_for_url(url) == None:
+        results = bias_checker(url)
+        add_sentiment_for_url(url, results[0], results[1], results[2])
+    else:
+        pass
 
-if __name__ == "__main__":
-    app.run()
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
