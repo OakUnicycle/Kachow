7178b59e7adc835f643ed79ad1153c6052f0b62f
Harry Vicaradge <harryvic_@outlook.com>
2025-10-18 15:48:26 +0100

    moved to one page

--- app.py
+++ app.py
@@ -2,25 +2,14 @@ from flask import Flask, render_template, request
 from bias_checker import bias_checker as bc
 app = Flask(__name__)
 
+
+url = 'hello'
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    url = request.form.get('url')
 
 
-@app.route("/upload", methods = ['POST']) # should be changed to name of website used in html
-def upload():
-    # if 'text' not in request.files:
-    #     return render_template('404.html')
-    
-    url = request.form.get("url") # gets the inputted url
-    bc(url) # bias checking function, takes in a website url, and returns a 'bias value'
-    
-    
-    return render_template('index.html')
-
-
-
-
+    return render_template('index.html', url = url)
 
 if __name__ == "__main__":
     app.run()
