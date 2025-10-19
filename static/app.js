2ad2d1dc1931408e5ea526d505248732c05b5376
FaDvvv <arthurbaverstock21@gmail.com>
2025-10-19 04:50:31 +0100

    i tried something

--- static/app.js
+++ static/app.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const toggleSwitch = document.getElementById('toggleSwitch');
+    const body = document.body;
+
+    toggleSwitch.addEventListener('click', () => {
+        body.classList.toggle('night');
+    });
+
     const slider_template = document.getElementById("slider_template");
     
 
@@ -136,48 +143,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
-/*
-function create_slider(slider) {
-  const template = document.getElementById("slider_template");
-  
-  const clone = template.content.cloneNode(true);
-
-  clone.querySelector(".slider_title").textContent = slider.title;
-
-  const sliderInput = clone.querySelector(".slider_input");
-  sliderInput.value = slider.value;
-
-  clone.querySelector(".slider_min").textContent = slider.min;
-  clone.querySelector(".slider_max").textContent = slider.max;
-
-  sliderInput.style.background = slider.gradient;
-
-  return clone;
-}
-
-const example_data = [
-  {
-    title: "Likelihood of Bias",
-    value: 50,
-    min: "0%",
-    max: "100%",
-    gradient: "linear-gradient(to right, white 100%)"
-  },
-  {
-    title: "Political Leaning",
-    value: 50,
-    min: "Left",
-    max: "Right",
-    gradient: "linear-gradient(to right, blue 50%, red 50%)"
-  }
-];
-
-const sideBar = document.getElementById("side_bar");
-
-example_data.forEach(example => {
-  const clone = create_slider(example)
-  console.log(clone);
-  sideBar.appendChild(clone);
-});*/
